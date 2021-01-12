11931562195e2e88a124ee07f6e70fe88dd4d055
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-01-12 00:35:54 -0500

    Update main.go

--- main.go
+++ main.go
@@ -1,9 +1,20 @@
 package main
 
-import (
-	"fmt"
-)
+import ()
 
 func main() {
-	fmt.Println("Hello, World!")
+	// Parse FLags
+	username := flag.String("username", "", "check services with given username")
+	flag.Parse()
+
+	if *username == "" {
+		// Read Username, if flags is empty
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print("\033[37;1mUsername:\033[0m ")
+		*username, _ = reader.ReadString('\n')
+	}
+
+	*username = strings.ToLower(strings.Replace(strings.Trim(*username, " \r\n"), " ", "", -1))
+
+	sherlock(*username)
 }
