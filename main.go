a5af2297c734145942af6e637094a647d9525156
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-06-11 00:33:59 -0400

    Update main.go

--- main.go
+++ main.go
@@ -127,14 +127,14 @@ func sherlock(username string) {
 }
 
 func main() {
-	if *username == "" {
+	if username == "" {
 		// Read Username, if flags is empty
 		reader := bufio.NewReader(os.Stdin)
 		fmt.Print("\033[37;1mUsername:\033[0m ")
-		*username, _ = reader.ReadString('\n')
+		username, _ = reader.ReadString('\n')
 	}
 
-	*username = strings.ToLower(strings.Replace(strings.Trim(*username, " \r\n"), " ", "", -1))
+	username = strings.ToLower(strings.Replace(strings.Trim(username, " \r\n"), " ", "", -1))
 
-	sherlock(*username)
+	sherlock(username)
 }
